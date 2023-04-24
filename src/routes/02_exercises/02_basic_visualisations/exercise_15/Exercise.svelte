43dd0e254109275a86280cc601493cc271fe2124
jcasadogp <juls.c99@gmail.com>
2023-04-24 13:32:44 +0200

    ex 15 and 16

--- src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
@@ -1,14 +1,15 @@
 <script>
     const width = 100;
     const height = 100;
-    const margin = { top: 5, right: 5, bottom: 5, left: 5 };
+    const margin = { top: 30, right: 30, bottom: 30, left: 30 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
   </script>
   
   <svg {width} {height}>
     <g transform="translate({margin.left}, {margin.top})">
-      <!--  -->
+      <rect x=0 y=0 width={innerWidth} height={innerHeight} />
+      <circle cx=innerWidth cy=innerHeight r=20 transform="translate({innerWidth},{innerHeight})"/>
     </g>
   </svg>
   
@@ -16,6 +17,12 @@
     svg {
       border: 1px solid black;
       border-radius: 5px;
+    }
+    rect {
+      fill: yellow
+    }
+    circle {
+      fill: steelblue
     }
   </style>
   
