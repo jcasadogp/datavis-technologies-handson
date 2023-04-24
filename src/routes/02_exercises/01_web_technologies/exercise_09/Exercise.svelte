9dd61b21cd7defc6a628b06c89441db819d01bea
jcasadogp <juls.c99@gmail.com>
2023-04-24 10:24:48 +0200

    ex 9

---
+++ src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
@@ -0,0 +1,12 @@
+<script>
+    function circumference(rad) {
+        return (2*Math.PI*rad).toFixed(2);;
+    }
+        
+    const circ = circumference(7);
+</script>
+
+
+<!--- HTML markup --->
+<p>The circumference of a circle with radius 7 is {circ}</p>
+
